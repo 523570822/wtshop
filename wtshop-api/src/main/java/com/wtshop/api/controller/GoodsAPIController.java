7975c626d00eee4e8248aacb48d5d5339b62038d
52357 <523570822@qq.com>
2018-07-18 13:26:03 +0800

    更新外包代码修改了6个文件

--- wtshop-api/src/main/java/com/wtshop/api/controller/GoodsAPIController.java
+++ wtshop-api/src/main/java/com/wtshop/api/controller/GoodsAPIController.java
@@ -305,12 +305,6 @@ public class GoodsAPIController extends BaseAPIController {
 				}
 			}
 
-			for(int i = 0; i < list.size() ; i++){
-				if(!list.get(i).getImages().contains("/")){
-					list.remove(i);
-				}
-			}
-
 			renderJson(ApiResult.success(imagePage));
 		}
 		else {
