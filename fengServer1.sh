00f1afff19ac4f40178b8fe632308192a2375247
52357 <523570822@qq.com>
2018-12-10 16:15:00 +0800

    增加订单实体字段增加发票相关

--- fengServer1.sh
+++ fengServer1.sh
@@ -11,7 +11,7 @@ echo "拉取git上代码"
 git pull origin   --force
 echo "通过maven自动编译代码"
 mvn -X install -Dmaven.test.skip=true
-echo "拷贝到Tomcat下   简化后拷贝编译后的代码到指定tomcat服务器中sudo systemctl restart tomcat  打包"
+echo "拷贝到Tomcat下   123简化后拷贝编译后的代码到指定tomcat服务器中sudo systemctl restart tomcat  打包"
 rsync -rtlvz --exclude 'wtshop.properties' /root/learngit/wtshop/wtshop-web/target/wtshop-web/*  /mrmf/tomcat_shop/webapps/ROOT
 rsync -rtlvz /root/learngit/mr_feng/wtshop.properties /mrmf/tomcat_shop/webapps/ROOT/WEB-INF/classes
 echo "开启tomcat tail -f /mrmf/tomcat_shop/logs/catalina.out"
