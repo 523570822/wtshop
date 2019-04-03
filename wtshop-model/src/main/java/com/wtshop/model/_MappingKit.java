3538636a799b5caba2b76f2baeed2c76e46ed465
52357 <523570822@qq.com>
2019-04-03 10:23:13 +0800

    1.优化wtshop/api/index.jhtml接口增加首页分类

--- wtshop-model/src/main/java/com/wtshop/model/_MappingKit.java
+++ wtshop-model/src/main/java/com/wtshop/model/_MappingKit.java
@@ -16,7 +16,6 @@ import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 public class _MappingKit {
 
 	public static void mapping(ActiveRecordPlugin arp) {
-		arp.addMapping("group_remind", "id", GroupRemind.class);
 		arp.addMapping("account", "id", Account.class);
 		arp.addMapping("act_introduce", "id", ActIntroduce.class);
 		arp.addMapping("activity", "id", Activity.class);
