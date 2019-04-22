08c557a917a9e999514305059436e18bead9ea98
52357 <523570822@qq.com>
2019-04-22 14:48:18 +0800

    优化查询规则

--- aDM.sh
+++ aDM.sh
@@ -6,13 +6,13 @@ sudo rm -rf /var/lib/tomcat/webapps/teacher/*
 cd /root/learngit/wtshop
 echo "??git"
 git pull origin   --force
-sleep 10
+sleep 5
 echo "??maven??"
 mvn -X install -Dmaven.test.skip=true
-sleep 10
+sleep 5
 #??
 #mvn install -Dmaven.test.skip=true -U -P dev
-echo "???Tomcat?"
+echo "??Tomcat?"
 # ??? ???????????tomcat????sudo systemctl restart tomcat  ??Tomcat;
 rsync -rtlvz --exclude 'wtshop.properties'  /root/learngit/wtshop/wtshop-web/target/wtshop-web/*    /var/lib/tomcat/webapps/teacher
 rsync -rtlvz /root/teacher/wtshop.properties /var/lib/tomcat/webapps/teacher/WEB-INF/classes
