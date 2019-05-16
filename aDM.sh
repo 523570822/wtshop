359d0e50c00b7cec27708bea314b10db104a3ced
52357 <523570822@qq.com>
2019-05-16 15:59:28 +0800

    增加扶持奖励计算逻辑,增加扶持奖励接口

--- aDM.sh
+++ aDM.sh
@@ -1,22 +1,22 @@
 echo "stop tomcat!"
- /mrmf/tomcat_shops/bin/shutdown.sh
-sleep 10
+systemctl stop service
+sleep 5
 echo "删除文件及缓存"
 sudo rm -rf /root/learngit/wtshop/wtshop-web/target/*
 sudo rm -rf /mrmf/tomcat_shops/webapps/ROOT/*
 cd /root/learngit/wtshop
 echo "拉取git上代码"
 git pull origin   --force
-sleep 10
+sleep 5
 echo "通过maven自动编译代码"
 mvn -X install -Dmaven.test.skip=true
 
-#打包
+#打包简化后 拷贝编译后的代码到指定tomcat服务器中sudo systemctl restart tomcat  启动Tomcat;
 #mvn install -Dmaven.test.skip=true -U -P dev
 echo "拷贝到Tomcat下"
-rsync -rtlvz --exclude 'wtshop.properties' /root/learngit/wtshop/wtshop-web/target/wtshop-web/*  /mrmf/tomcat_shops/webapps/ROOT  ;   简化后 拷贝编译后的代码到指定tomcat服务器中sudo systemctl restart tomcat  启动Tomcat;
+rsync -rtlvz --exclude 'wtshop.properties' /root/learngit/wtshop/wtshop-web/target/wtshop-web/*  /mrmf/tomcat_shops/webapps/ROOT
 rsync -rtlvz /mrmf/tomcat_shops/wtshop-web/WEB-INF/classes/wtshop.properties /mrmf/tomcat_shops/webapps/ROOT/WEB-INF/classes
 
 echo "开启tomcat"
-sudo /mrmf/tomcat_shops/bin/startup.sh
+systemctl start service
 #tail -f /mrmf/tomcat_shops/logs/catalina.out
