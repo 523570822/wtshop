3017bcfe6aea86667af20d66c827ae617d79b778
52357 <523570822@qq.com>
2019-05-16 15:49:08 +0800

    增加扶持奖励计算逻辑,增加扶持奖励接口

--- wtshop-dao/src/main/java/com/wtshop/dao/PrestoreDao.java
+++ wtshop-dao/src/main/java/com/wtshop/dao/PrestoreDao.java
@@ -1,10 +1,6 @@
 package com.wtshop.dao;
-
-import com.sun.scenario.effect.impl.prism.PrDrawable;
 import com.wtshop.model.Prestore;
-
 import java.util.List;
-
 /**
  * Created by sq on 2017/9/15.
  */
