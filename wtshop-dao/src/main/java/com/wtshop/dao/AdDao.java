c694491e3d5011354fcc15f8a78f0ad4d176b1c9
52357 <523570822@qq.com>
2019-06-26 13:26:53 +0800

    返回上一页并刷新

--- wtshop-dao/src/main/java/com/wtshop/dao/AdDao.java
+++ wtshop-dao/src/main/java/com/wtshop/dao/AdDao.java
@@ -21,7 +21,7 @@ public class AdDao extends OrderEntity<Ad> {
      */
 
     public List<Ad> findAdList(Long id) {
-        String sql = "SELECT t.target_path, t.urltype ,a.title ,a.path, a.param FROM ad a LEFT JOIN target_path t on a.target_id = t.id WHERE a.ad_position_id =" + id;
+        String sql = "SELECT t.target_path,a.target_id, t.urltype ,a.title ,a.path, a.param FROM ad a LEFT JOIN target_path t on a.target_id = t.id WHERE a.ad_position_id =" + id;
         List<Ad> ads = modelManager.find(sql);
         return ads;
     }
