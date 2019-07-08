51067106624ce0ca5f30c6a886186bd061cb29ba
52357 <523570822@qq.com>
2019-07-08 11:19:14 +0800

    返回上一页并刷新

--- aDM.sh
+++ aDM.sh
@@ -6,7 +6,7 @@ rm -rf /root/learngit/wtshop/wtshop-web/target/*
 rm -rf /mrmf/apache-tomcat-8.5.42/webapps/ROOT/*
 cd /root/learngit/wtshop
 echo "拉取git上代码"
-git pull origin   --force
+
 echo "通过maven自动编译代码"
 mvn -X install -Dmaven.test.skip=true
 echo "拷贝到Tomcat下   简化后拷贝编译后的代码到指定tomcat服务器中sudo systemctl restart tomcat  打包"
@@ -16,3 +16,5 @@ chmod 777 /usr/share/tomcat/webapps/wtshop/upload
 chmod 777 /var/lib/tomcat/webapps/wtshop/WEB-INF/classes/*
 echo "开启tomcat tail -f /mrmf/apache-tomcat-8.5.42/logs/catalina.out"
 /mrmf/apache-tomcat-8.5.42/bin/startup.sh
+
+
