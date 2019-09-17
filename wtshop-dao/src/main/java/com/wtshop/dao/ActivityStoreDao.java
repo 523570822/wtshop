3b09a26e583f55113767f6ca8e9681c603619e83
52357 <523570822@qq.com>
2019-09-17 17:40:20 +0800

    返回上一页并刷新

---
+++ wtshop-dao/src/main/java/com/wtshop/dao/ActivityStoreDao.java
@@ -0,0 +1,42 @@
+package com.wtshop.dao;
+
+import com.jfinal.plugin.activerecord.Page;
+import com.wtshop.Pageable;
+import com.wtshop.model.Certificates;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by 蔺哲 on 2017/5/25.
+ */
+public class ActivityStoreDao extends BaseDao<Certificates> {
+    public ActivityStoreDao(){super(Certificates.class);}
+
+    public Certificates queryByMemberId(Long memberId){
+        String sql = "select * from certificates where member_id = " + memberId;
+        return modelManager.findFirst(sql);
+    }
+
+    /**
+     * 根据状态分页查询
+     */
+    public Page<Certificates> findShenHePage(String username, String create_date, Pageable pageable, Integer type){
+
+            String sql = "From certificates where 1 = 1";
+            if(type != null && 4 != type){
+
+                sql +=" AND state = " + type;
+            }
+            if(!StringUtils.isEmpty(username)){
+                sql +=" AND name LIKE " + "'%" +username+"%'";
+            }
+            if(!StringUtils.isEmpty(create_date)){
+                sql +=" AND create_date  >= '" + create_date + "'  ";
+            }
+            sql += " order by create_date desc ";
+            String select = "select * ";
+            return modelManager.paginate(pageable.getPageNumber(), pageable.getPageSize(), select, sql);
+
+
+
+    }
+}
--- wtshop-model/src/main/java/com/wtshop/model/_MappingKit.java
+++ wtshop-model/src/main/java/com/wtshop/model/_MappingKit.java
@@ -16,6 +16,7 @@ import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 public class _MappingKit {
 
 	public static void mapping(ActiveRecordPlugin arp) {
+		arp.addMapping("activity_store", "id", ActivityStore.class);
 		arp.addMapping("special_coupon", "id", SpecialCoupon.class);
 		arp.addMapping("account", "id", Account.class);
 		arp.addMapping("full_reduction", "id", FullReduction.class);
